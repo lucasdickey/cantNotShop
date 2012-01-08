0951f933ae08fc32675e991e9c82275d52f45b8b
Lucas Dickey <lucas@lucasdickey.com>
2012-01-07 21:55:29 -0800

    Updating various assets for the most part.

--- src/com/CantNotShop/CantNotShopActivity.java
+++ src/com/CantNotShop/CantNotShopActivity.java
@@ -2,12 +2,15 @@ package com.CantNotShop;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class CantNotShopActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.home_screen);
     }
 }
+
